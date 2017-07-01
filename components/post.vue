c331f0478636a43bd069e9162e05ca6e2ea9bd00
LiTingjun <739142578@qq.com>
2017-07-02 00:06:46 +0800

    Use Vue component file

---
+++ components/post.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <div class="post-info">
+            <h1 id="post-title">BlogTitle</h1>
+            <div>url: {{ $route.params.url }}</div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
--- router.js
+++ router.js
@@ -1,10 +1,12 @@
+import Post from './components/post.vue'
+
 const index={
     template: "<h2>Blog index</h2>"
 };
 
-const Post = {
-    template: "<div><p>Blog detail</p><div>{{ $route.params.url }}</div></div>"
-};
+// const Post = {
+//     template: "<div><p>Blog detail</p><div>{{ $route.params.url }}</div></div>"
+// };
 
 const Error = {
     template: "<div>ERROR: 404</div>"
