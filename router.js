7c4524486119e4749cfdbafdfce1733254d387b2
LiTingjun <739142578@qq.com>
2017-07-01 23:44:36 +0800

    switch to vue-router

---
+++ router.js
@@ -0,0 +1,25 @@
+const index={
+    template: "<h2>Blog index</h2>"
+};
+
+const Post = {
+    template: "<div><p>Blog detail</p><div>{{ $route.params.url }}</div></div>"
+};
+
+const Error = {
+    template: "<div>ERROR: 404</div>"
+}
+
+const router = new VueRouter({
+    routes: [
+        { path: '/post/:url', component: Post},
+        { path: '', component:index},
+        { path: '*', component:Error}
+    ]
+});
+
+const app = new Vue({
+    el: "#app",
+    router: router
+});
+
--- static/js/main.js
+++ static/js/main.js
@@ -1,18 +1,23 @@
 var blogapp = new Vue({
     el: "#content",
     data: {
-        post_list:[]
+        post_list:[],
+        post:{
+            title:"",
+            author:"",
+            time:"",
+            category:"",
+            tags:[],
+            text:""
+        }
     },
     methods: {
         fetch_list: function(){
-            console.log("Fetch");
             $.ajax({
                 method: "get",
                 url: '/posts/list',
                 contentType: "text/plain",
-                dataType: "text/plain",
                 success: function (data,status) {
-                            console.log(status);
                             blogapp.$data['post_list']=JSON.parse(data);
                         }
             });
@@ -20,9 +25,51 @@ var blogapp = new Vue({
     },
     mounted: function () {
         $(document).ready(function () {
-            console.log("Ready");
             blogapp.fetch_list();
         });
 
     }
 });
+
+function router_hide(ctx,next) {
+    $(".router-page").each(function () {
+        if($(this).hasClass('hide')){
+        }else{
+            $(this).addClass('hide');
+        }
+        next();
+    });
+}
+function router_index() {
+    // if($(this).hasClass('hide')){
+    //     $(this).removeClass('hide');
+    // }
+    console.log('index');
+    $("#router-index").removeClass('hide');
+};
+
+function router_post() {
+    // if($(this).hasClass('hide')){
+    //     $(this).removeClass('hide');
+    // }
+    console.log('post');
+    $("#router-post").removeClass('hide');
+};
+
+function router_default(ctx, next) {
+    // if($(this).hasClass('hide')){
+    //     $(this).removeClass('hide');
+    // }
+    console.log('default')
+    $("#router-default").removeClass('hide');
+    next();
+}
+// page('/*', router_hide);
+page('/',router_hide , router_index);
+page('/post/:url',router_hide , router_post);
+// page('*',router_hide , router_default);
+page();
+
+// page('/post/:url', router_hide, router_post);
+// page('*', router_hide, router_default);
+// page();
